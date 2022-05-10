e4ab1a7b030a9144386a63821a5bb0ef62f6d1f5
sam-levin <sam.ky.levin@gmail.com>
2022-05-10 07:14:59 -0700

    adding initial user post routes and report post and get routes

--- controllers/api/post-routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-//const { Post, User, Vote , Comment} = require('../../models');
-const sequelize = require('../../config/connection')
-
-
-
-
-module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,34 @@
 const router = require('express').Router();
 const { json } = require('express/lib/response');
-//const { User, Post, Vote } = require('../../models');
+const { User, Report } = require('../../models');
+
+
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: {exclude: ['password']}
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        })
+});
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData)
+      })
+    })
+});
 
 module.exports = router;
