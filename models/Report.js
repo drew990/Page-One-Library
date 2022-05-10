8ed79dddc31e43dfee405a7bbf6cb5c74ac3a35f
sam-levin <sam.ky.levin@gmail.com>
2022-05-09 22:22:33 -0700

    adding models

---
+++ models/Report.js
@@ -0,0 +1,59 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const { report } = require('../controllers');
+
+// create Post model
+
+class Report extends Model {}
+
+Report.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        report_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              isURL: true
+            }
+        },
+        report_author: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        report_content: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        report_score: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'user',
+              key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'report'
+    }
+
+)
+
+
+module.exports = Report;
+
