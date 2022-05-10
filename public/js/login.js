0fc269f35a874a11391716424dbeb498b09ff0f7
Andrew Banagas <andrewbanagas40@gmail.com>
2022-05-10 13:45:07 -0700

    Updated Login JS and Handlebar

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,7 @@
 // Login JS handler
 async function loginFormHandler(e) {
+  // Prevent default action from happening
+  // Note: Prevents redirect until login is replace
   e.preventDefault();
 
   // Get Login information
@@ -14,13 +16,51 @@ async function loginFormHandler(e) {
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
     });
-  }
 
-  // If response is valid, it'll load the home screen
-  if (Response.ok) {
-    document.location.replace("/");
-  } else {
-    // Let user know they failed to log in
-    alert("Failed to log in.");
+    // If response is valid, it'll load the home screen
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      // Let user know they failed to log in
+      alert("Failed to log in.");
+    }
   }
 }
+
+async function signupFormHandler(e) {
+  // Prevent default action from happening
+  // Note: Prevents redirect until sign up is confirm
+  e.preventDefault();
+
+  // Get Sign up info
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  // Checks if info is filled
+  if (username && email && password) {
+    // Get response from login info
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to sign up.");
+      alert(response.statusText);
+    }
+  }
+}
+
+// Listens for the login click
+document
+  .getElementById("login-btn")
+  .addEventListener("click", loginFormHandler);
+
+// listens for sign up click
+document
+  .getElementById("signup-btn")
+  .addEventListener("click", signupFormHandler);
