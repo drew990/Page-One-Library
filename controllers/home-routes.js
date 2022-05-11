93369d258fa712b904674fe3c04cee615b7d7dd3
sam-levin <sam.ky.levin@gmail.com>
2022-05-11 07:04:52 -0700

    committing for merge

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection')
-//const { Post , User, Comment } = require('../models')
+const { Report , User, Comment } = require('../models')
 
 
-router.get('/', (req, res) => {
-    console.log("Test!")
-    res.send("tests")
-})
 
 module.exports = router;
