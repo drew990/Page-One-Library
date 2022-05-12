8fc54cf70db42819acf2841b41e4e34c56450d6d
sam-levin <sam.ky.levin@gmail.com>
2022-05-12 07:42:08 -0700

    changing seeds files

--- seeds/report-seeds.js
+++ seeds/report-seeds.js
@@ -2,42 +2,55 @@ const { Report } = require('../models');
 
 const reportdata = [
   {
-    title: 'Harry Potter and the poopy butt',
-    report_url: 'urlid.com',
-    report_author: null,
-    report_content: 'I loved it!!!',
-    report_score: 5,
-    user_id: 1,
+[
+	{
+		"title": "Book Test",
+		"created_at": "2022-05-12T13:56:04.000Z",
+		"report_author": "Author Test",
+		"score": 1,
+		"user_id": 6,
+		"url": "meh.com",
+		"content": "It was alright"
+		},
+  {
+    "title": "Clifford the Big Red Dog",
+    "created_at": "2022-05-12T13:56:04.000Z",
+    "report_author": "Author Test",
+    "score": 5,
+    "user_id": 6,
+    "url": "meh.com",
+    "content": "It was alright"
   },
   {
-    title: 'Harry Potter and the poopy butt',
-    report_url: 'urlid.com',
-    report_author: null,
-    report_content: 'I loved it!!!',
-    report_score: 5,
-    user_id: 1,
-  },  {
-    title: 'Harry Potter and the poopy butt',
-    report_url: 'urlid.com',
-    report_author: null,
-    report_content: 'I loved it!!!',
-    report_score: 5,
-    user_id: 1,
-  },  {
-    title: 'Harry Potter and the poopy butt',
-    report_url: 'urlid.com',
-    report_author: null,
-    report_content: 'I loved it!!!',
-    report_score: 5,
-    user_id: 1,
-  },  {
-    title: 'Harry Potter and the poopy butt',
-    report_url: 'urlid.com',
-    report_author: null,
-    report_content: 'I loved it!!!',
-    report_score: 5,
-    user_id: 1,
-  },]
+    "title": "Clifford the Big Red Dog",
+    "created_at": "2022-05-12T13:56:04.000Z",
+    "report_author": "Author Test",
+    "score": 3,
+    "user_id": 6,
+    "url": "meh.com",
+    "content": "It was alright"
+  },
+  {
+    "title": "Book Test",
+    "created_at": "2022-05-12T13:56:04.000Z",
+    "report_author": "Author Test",
+    "score": 4,
+    "user_id": 3,
+    "url": "meh.com",
+    "content": "It was alright"
+  },
+  {
+    "title": "Book Test",
+    "created_at": "2022-05-12T13:56:04.000Z",
+    "report_author": "Author Test",
+    "score": 4,
+    "user_id": 4,
+    "url": "meh.com",
+    "content": "It was alright"
+  }
+]
+}]
+
 const seedReports = () => Post.bulkCreate(reportdata);
 
 module.exports = seedReports;
