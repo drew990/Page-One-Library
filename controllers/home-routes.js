ec05641bc04c04537adbb74708e71b21ca1a777a
Andrew Banagas <andrewbanagas40@gmail.com>
2022-05-11 21:16:58 -0700

    Updated handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,105 +1,112 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection')
-const { Report , User, Comment } = require('../models')
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Report, User, Comment } = require("../models");
 
+router.get("/", (req, res) => {
+  console.log(req.session);
+  Report.findAll({
+    // Attribution not needed and can just include model
 
-router.get('/', (req, res) => {
-    console.log(req.session)
-    Report.findAll ({
+    include: [
+      {
+        model: Comment,
         attributes: [
-            'id',
-            'report_url',
-            'title',
-            'created_at',
-            'report_author',
-            'report_score',
-            'user_id',
-        ], 
-        include:[
-            {
-              model: Comment,
-              attributes: ['id', 'comment_text','report_id', 'user_id', 'created_at'],
-              include: {
-                model: User,
-                attributes: ['username']
-              }
-            },
-            {
-              model: User,
-              attributes: ['username']
-            }
-          ]
+          "id",
+          "comment_text",
+          "report_id",
+          "user_id",
+          "created_at",
+        ],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbReportData) => {
+      const reports = dbReportData.map((report) => report.get({ plain: true }));
+      res.render("homepage", console.log("Look AT ME", reports), {
+        // reports is the variable that I'll be grabbing
+        reports,
+        loggedIn: req.session.loggedIn,
+      });
     })
-    .then(dbReportData => {
-        const reports = dbReportData.map(report => report.get({ plain:true}))
-        res.render('homepage', {
-          reports,
-          loggedIn: req.session.loggedIn
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('login');
-  });
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
 
-  router.get('/report/:id', (req, res) => {
-    Report.findOne({
-      where: {
-        id: req.params.id
-      },
-      attributes: [
-        'id',
-            'report_url',
-            'title',
-            'created_at',
-            'report_author',
-            'report_score',
-            'user_id',
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'report_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
+  // Need a login check to make sure password and email match
+
+  res.render("login");
+});
+
+router.get("/report/:id", (req, res) => {
+  Report.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [
+      "id",
+      "report_url",
+      "title",
+      "created_at",
+      "report_author",
+      "report_score",
+      "user_id",
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: [
+          "id",
+          "comment_text",
+          "report_id",
+          "user_id",
+          "created_at",
+        ],
+        include: {
           model: User,
-          attributes: ['username']
-        }
-      ]
-    })
-      .then(dbReportData => {
-        if (!dbReportData) {
-          res.status(404).json({ message: 'No report found with this id' });
-          return;
-        }
-  
-        // serialize the data
-        const report = dbReportData.get({ plain: true });
-  
-        // pass data to template
-        res.render('single-report', { 
-          report,
-          loggedIn: req.session.loggedIn 
-        });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbReportData) => {
+      if (!dbReportData) {
+        res.status(404).json({ message: "No report found with this id" });
+        return;
+      }
 
-  module.exports = router
+      // serialize the data
+      const report = dbReportData.get({ plain: true });
+
+      // pass data to template
+      res.render("single-report", {
+        report,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
