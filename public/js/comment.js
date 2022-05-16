c741add469b77b81770a1ebea6a9770c781606aa
Andrew Banagas <andrewbanagas40@gmail.com>
2022-05-15 23:55:18 -0700

    New code

---
+++ public/js/comment.js
@@ -0,0 +1,32 @@
+// Comment JS handler
+async function commentFormHandler(e) {
+  // Prevent default action from happening
+  // Note: Prevents redirect until login is replace
+  e.preventDefault();
+
+  // Get Login information
+  const comment = document.querySelector("#book-comment").value.trim();
+
+  // Checks if comment is filled
+  if (comment) {
+    // Get response from comment info
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ comment }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    // If response is valid, it'll refresh the screen
+    if (response.ok) {
+      document.location.replace("/report/{{id}}");
+    } else {
+      // Let user know they failed to comment
+      alert("Failed to submit comment.");
+    }
+  }
+}
+
+// listens for sign up click
+document
+  .getElementById("comment-btn")
+  .addEventListener("click", commentFormHandler);
