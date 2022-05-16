dd1d7211c29134226c8fde1a725831ac138ea2c3
nitishd22 <nitishd@gmail.com>
2022-05-15 21:14:13 -0700

    commit route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -62,7 +62,7 @@ router.get('/:id', (req,res) =>{
     });
 });
 
-router.post('/login', (req, res) => {
+router.post('/', (req, res) => {
     User.create({
         username: req.body.username,
         email: req.body.email,
